e3062b0f1f626bf02b3d12f80359683e81460022
David-VTUK <david.holder@gmail.com>
2024-07-26 13:29:55 +0000

    Adding CIlium Config for BGP

--- 02-cni/installCilium.go
+++ 02-cni/installCilium.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/apiextensions"
 	helmv4 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/helm/v4"
+	v1 "github.com/pulumi/pulumi-kubernetes/sdk/v4/go/kubernetes/meta/v1"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
 )
 
 func installCilium(ctx *pulumi.Context, ciliumVersion string) error {
 
-	_, err := helmv4.NewChart(ctx, "helm-chart-cilium", &helmv4.ChartArgs{
+	ciliumChart, err := helmv4.NewChart(ctx, "helm-chart-cilium", &helmv4.ChartArgs{
 		Chart:   pulumi.String("cilium"),
 		Version: pulumi.String(ciliumVersion),
 		RepositoryOpts: &helmv4.RepositoryOptsArgs{
@@ -18,10 +21,10 @@ func installCilium(ctx *pulumi.Context, ciliumVersion string) error {
 			"k8sServiceHost": pulumi.String("172.16.10.220"),
 			"k8sServicePort": pulumi.String("6443"),
 			"annotations": pulumi.Map{
-				"meta.helm.sh/release-name": pulumi.String("helm-chart-cilium"),
+				"meta.helm.sh/release-name":      pulumi.String("helm-chart-cilium"),
 				"meta.helm.sh/release-namespace": pulumi.String("kube-system"),
 			},
-			
+
 			"hubble": pulumi.Map{
 				"enabled": pulumi.String("true"),
 			},
@@ -30,8 +33,54 @@ func installCilium(ctx *pulumi.Context, ciliumVersion string) error {
 					"clusterPoolIPv4PodCIDRList": pulumi.StringArray{pulumi.String("10.42.0.0/16")},
 				},
 			},
+			"bgpControlPlane": pulumi.Map{
+				"enabled": pulumi.String("true"),
+			},
 		},
 	})
+
+	if err != nil {
+		return err
+	}
+
+	// Set up BGP Peering with Mikrotik Router
+	_, err = apiextensions.NewCustomResource(ctx, "bgp-peering-policy", &apiextensions.CustomResourceArgs{
+		ApiVersion: pulumi.String("cilium.io/v2alpha1"),
+		Kind:       pulumi.String("CiliumBGPPeeringPolicy"),
+		Metadata: &v1.ObjectMetaArgs{
+			Name: pulumi.String("default"),
+		},
+		OtherFields: kubernetes.UntypedArgs{
+			"spec": pulumi.Map{
+				"virtualRouters": pulumi.Map{
+					"localASN": pulumi.Int(64512),
+				},
+				"neighbors": pulumi.Array{
+					pulumi.Map{
+						"peerAddress": pulumi.String("172.16.10.1"),
+						"peerASN":     pulumi.Int(64512),
+					},
+				},
+			},
+		},
+	}, pulumi.DependsOn([]pulumi.Resource{ciliumChart}))
+
+	_, err = apiextensions.NewCustomResource(ctx, "bgp-ip-pool", &apiextensions.CustomResourceArgs{
+		ApiVersion: pulumi.String("cilium.io/v2alpha1"),
+		Kind:       pulumi.String("CiliumLoadBalancerIPPool"),
+		Metadata: &v1.ObjectMetaArgs{
+			Name: pulumi.String("default"),
+		},
+		OtherFields: kubernetes.UntypedArgs{
+			"spec": pulumi.Map{
+				"cidrs": pulumi.StringArray{
+					pulumi.String("10.50.20.0/24"),
+				},
+			},
+		},
+	}, pulumi.DependsOn([]pulumi.Resource{ciliumChart}))
+
+	// Set up Routes to Advertise
 
 	if err != nil {
 		return err
